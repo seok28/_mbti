02679e6a3608c562bbc6d98f8624ab7f57893501
seok <seokjoonsoo@gmail.com>
2022-06-13 15:24:43 +0900

    final pro

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from './pages/Home';
 import Question from './pages/Question';
 import Result from './pages/Result';
 import  {Routes,Route } from 'react-router-dom';
-import './App.css';
+
 
 function App() {
   return (
